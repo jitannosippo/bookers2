da34a40f247f2e2879b1f207211d37a4567e171e
jitannosippo <kanamakigai@gmail.com>
2022-09-13 21:48:33 +0000

    finished commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+
+
+  def index
+    @book = Book.new
+    @users = User.all
+    @user = User.find(current_user.id)
+  end
+
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+      render "edit"
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.update(user_params)
+      flash[:success] = 'You have updated user successfully.'
+      redirect_to user_path(@user.id)
+    else
+      render:edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/about' => 'homes#about'
+  root to:"homes#top"
+  resources:users,only:[:new,:index,:show,:edit,:update]
+  resources:books,only:[:new,:create,:index,:show,:edit,:update,:destroy]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
